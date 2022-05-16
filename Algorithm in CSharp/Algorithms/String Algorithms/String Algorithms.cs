622abd316adb6ccbeec097c7b2763f6dae0cfeb4
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-05-17 00:11:10 +0430

    check for lower and upper for string

---
+++ Algorithm in CSharp/Algorithms/String Algorithms/String Algorithms.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_in_CSharp.Algorithms.String_Algorithms
+{
+    public class String_Algorithms
+    {
+        public Boolean IsUpperCase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+
+        public Boolean IsLowerCase(string s)
+        {
+            return s.All(char.IsLower);
+        }
+    }
+}
--- Algorithm in CSharp/Program.cs
+++ Algorithm in CSharp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Algorithm_in_CSharp.Algorithms.String_Algorithms;
+
+Console.WriteLine("Hi form the program!");
+
+String_Algorithms stringAlgo = new String_Algorithms();
+
+var lower = stringAlgo.IsLowerCase("hello");
+var upper = stringAlgo.IsUpperCase("HELLO");
+Console.WriteLine(lower);
+Console.WriteLine(upper);
