a4b9645c205d00176aeb3c3d76d35b618fd4fffc
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-05-20 11:49:33 +0430

    Built in funition for stack created

---
+++ Algorithm in CSharp/Algorithms/Qeue And Stack/CustomStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_in_CSharp.Algorithms.Qeue_And_Stack
+{
+    public class CustomStack
+    {
+        public void BultInStack()
+        {
+            Stack<string> stack = new Stack<string>();
+            Console.WriteLine("Start main ");
+            stack.Push("Main");
+
+            Console.WriteLine("Start ProccessBuilder ");
+            stack.Push("ProcessBuilder");
+
+            Console.WriteLine("Start CallExternalProcess");
+            stack.Push("CallExternalProcess");
+
+            Console.WriteLine("End " + stack.Pop());
+
+            Console.WriteLine("Start CallEnternalProcess");
+            stack.Push("CallEnternalProcess");
+
+            Console.WriteLine("End " + stack.Pop());
+            Console.WriteLine("End " + stack.Pop());
+            Console.WriteLine("End " + stack.Pop());
+        }
+    }
+}
